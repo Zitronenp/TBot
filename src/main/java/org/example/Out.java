fd4a98fbc336ae7f4f9417bd2c048776648f5917
Zitronenp <it222003@yandex.ru>
2022-11-15 21:25:33 +0500

    Step1

---
+++ src/main/java/org/example/Out.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Out {
+
+    public String outputData;
+
+    public Out() {}
+    public Out(String data)
+    {
+        outputData = data;
+    }
+
+    public void consPrint()
+    {
+        System.out.println(outputData);
+    }
+}
